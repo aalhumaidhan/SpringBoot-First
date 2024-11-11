448d8db0846f097362d65e474022c54dbf7683cf
Abdullah Alhumaidhan <abdullahkh56@gmail.com>
2024-11-11 09:51:33 +0300

    Final commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,33 +2,17 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.ArrayList;
-
 @RestController
 public class DemoController {
 
-    List<Contact> contactsList = new ArrayList<>();
-
-    @PostMapping("/addContact")
-    String addContact(@RequestBody Contact contact) {
-        for (Contact c : contactsList) {
-            if (contact.getEmail().equals(c.getEmail())) {
-                return "Contact already exists with this email!";
-            }
-        }
-        contactsList.add(contact);
-        return "Contact added successfully!";
+    @GetMapping("/greet")
+    public String greet(@RequestParam String name) {
+        return "Hello, " + name;
     }
 
-    @GetMapping("/getContactDetails")
-    public Contact getContactDetails(@RequestParam String name) {
-        for (Contact contact : contactsList) {
-            if (contact.getName().equals(name)) {
-                return contact;
-            }
-        }
-        throw new IllegalArgumentException("Contact not found!");
+    @PostMapping("/farewell")
+    public String farewell(@RequestBody Person person) {
+        return "Goodbye, " + person.getName() + "!";
     }
 
 }
